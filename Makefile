5d5325e8a75b5a36a6f911973db663c9f05b5f6e
Erik Zachrisson <erik@zachrisson.info>
2016-11-19 09:06:14 +0100

    Initial clone

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ CFLAGS = -Wall
 OUTPUT = DelcomLinux
 RM = rm -f
 
-all: delcom_light delcom_cycle_colors
+all: delcom_light delcom_cycle_colors delcom_red_flasher
 
 delcom_light: DelcomLinux.c
 	$(CC) $(CFLAGS) DelcomLinux.c -lhidapi-libusb -o $@
@@ -12,5 +12,8 @@ delcom_light: DelcomLinux.c
 delcom_cycle_colors: delcom_cycle_colors.c
 	$(CC) $(CFLAGS) delcom_cycle_colors.c -lhidapi-libusb -o $@
 
+delcom_red_flasher: delcom_red_flasher.c
+	$(CC) $(CFLAGS) delcom_red_flasher.c -lhidapi-libusb -o $@
+
 clean:
-	$(RM) $(OUTPUT) delcom_light delcom_cycle_colors
+	$(RM) $(OUTPUT) delcom_light delcom_cycle_colors delcom_red_flasher
---
+++ delcom_red_flasher.c
@@ -0,0 +1,45 @@
+// Cycles through the colors
+// Copyright Erik Zachrisson: erik@zachrisson.info
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+#include "delcom_linux.h"
+
+static HIDPacketStruct my_packet;
+
+int write_hid(hid_device *hnd, uint8_t data) {
+  // Write 8 byte command
+  my_packet.tx.major_cmd = WRITE_PORT_CMD;
+  // Write to port 1 command
+  my_packet.tx.minor_cmd = 2;
+  // Data to write to port1
+  my_packet.tx.data_lsb = data;
+  // Send it
+  return hid_send_feature_report(hnd, my_packet.data, 8);
+}
+
+int main(int argc, char* argv[]) {
+  hid_device *handle;
+
+  // Open the device using the VID, PID,
+  handle = hid_open(DELCOM_HID_VID, DELCOM_HID_PID, NULL);
+  if (!handle) {
+    printf("Error: Unable to open device. (%d:%s)\n", errno, strerror(errno));
+    return errno;
+  }
+
+  while (true) {
+    write_hid(handle, GREEN_COLOR);
+    usleep(200000);
+    write_hid(handle, RED_COLOR);
+    usleep(200000);
+    write_hid(handle, YELLOW_COLOR);
+    usleep(200000);
+  }
+  return 0;
+}
